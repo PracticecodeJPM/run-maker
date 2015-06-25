8179f0654de867a683918fa0a3fdca95a0883fad
johnpmitsch <johnpmitsch@gmail.com>
2015-06-24 21:58:33 -0400

    added json api for schedule and seed data

--- marathon_engine/db/seeds.rb
+++ marathon_engine/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+run_options = ["short run", "rest", "long run", "medium run", "cross train"]
+
+100.times do |i|
+  Schedule.create(monday: run_options.sample,
+                  tuesday: run_options.sample,
+                  wednesday: run_options.sample,
+                  thursday: run_options.sample,
+                  friday: run_options.sample,
+                  saturday: run_options.sample,
+                  sunday: run_options.sample
+                 )
+end
